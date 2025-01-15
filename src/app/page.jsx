911e76fcb9e8d59b9e63ba396130f4387827c310
EldarMemetov <eldarm9347@gmail.com>
2025-01-15 18:53:14 +0100

    change HOME

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,9 @@
+import styles from "./home.module.css";
+
 export default function Home() {
   return (
-    <main>
-      <h1>Welcome to SSR App</h1>
+    <main className={styles.main}>
+      <h1 className={styles.title}>Welcome to SSR App</h1>
     </main>
   );
 }
